d3d019d68c927c7af8e86c456034fa23ad2015fd
kary5678 <60301055+kary5678@users.noreply.github.com>
2022-04-10 18:39:11 -0500

    Patch for JP names having spaces, style improvements

--- scrape_translations.R
+++ scrape_translations.R
@@ -13,37 +13,39 @@ location2 <- str_locate(names_string,'</ul>
 data <- str_sub(names_string,location1[2],location2[1]) #subset to just the translation data
 
 row_entries <- strsplit(data, split="</li>\n<li>")[[1]] #separate each monster to their own line
-id_2names <- unlist(strsplit(row_entries, split="(?<=\\d{4})\\. ", perl=TRUE)) #split id and combined na+jp name
-df <- as.data.frame(matrix(id_2names, ncol=2, byrow=TRUE)) #all of the data...
-df2 <- subset(df, V2!="?" & V2!="? [Original Art Lina]") #remove entries that haven't been translated/added
-df2$V2 <- str_replace(df2$V2, "&amp;", "&") #adjust text formatting
+id_names <- unlist(strsplit(row_entries, split="(?<=\\d{4})\\. ", perl=TRUE)) #split id and combined na+jp name
+all_data_df <- as.data.frame(matrix(id_names, ncol=2, byrow=TRUE))
+all_data_df2 <- subset(all_data_df, V2!="?" & V2!="? [Original Art Lina]") #remove entries that haven't been translated/added
+all_data_df2$V2 <- str_replace_all(all_data_df2$V2, "&amp;", "&") #adjust text formatting
 
 #extract numeric ids from new_card_announce post, replace text for each wave of new cards
-char_ids <- strsplit("
-    [6998] 至福の魔女・ポンノ
-    [6999] バレンタインの双魚神・アルレシャ
-    [7000] 怪しいバレンタインチョコレート", split="\\s+")[[1]][-1]
+new_cards <- strsplit("】
+    [8726] 名探偵コナン 灰原哀セレクション上巻【灰原哀】
+    [8727] 名探偵コナンvs.怪盗キッド完全版1巻【怪盗キッド】
+    [8678] 探偵・江戸川コナン
+    [8679] 工藤新一
+    [8680] 工藤新一【原作版】
+    [8681] 名探偵コナン 工藤新一セレクションｖｏｌ．１【工藤新一】", split="\n")[[1]][-1]
 
-dff <- as.data.frame(matrix(char_ids, ncol=2, byrow=TRUE))
-dff$V1 <- str_replace(dff$V1, "\\[", "")
-dff$V1 <- str_replace(dff$V1, "\\]", "")
-df3 <- subset(df2, V1 %in% dff$V1) #subset scraped data with translations to those of interest
+new_cards_df <- as.data.frame(matrix(unlist(str_split(new_cards, pattern="\\]", n=2)), ncol=2, byrow=TRUE))
+new_cards_df$V1 <- str_replace_all(new_cards_df$V1, "[^0-9]", "")
+translated <- subset(all_data_df2, V1 %in% new_cards_df$V1) #subset scraped data with translations to those of interest
 
 #split na+jp name into na and jp fields
-na_jp_strings <- unlist(str_split(df3$V2, pattern="\\((?=[^(]+$)")) #split original single string with both names
+na_jp_strings <- unlist(str_split(translated$V2, pattern="\\((?=[^(]+$)")) #split original single string with both names
 na_jp_names <- as.data.frame(matrix(na_jp_strings, ncol=2, byrow=TRUE))
-df3$V2 <- na_jp_names$V1
-df3$V3 <- na_jp_names$V2
+translated$V2 <- na_jp_names$V1
+translated$V3 <- na_jp_names$V2
 
-#merge new_card_announce stuff (dff) with relevant scraped to check
-final <- full_join(dff, df3, by="V1")
+#merge new_card_announce stuff (new_cards_df) with relevant scraped to check
+final <- full_join(new_cards_df, translated, by="V1")
 final <- final[colnames(final)[c(1,3,2,4)]]
 colnames(final) <- c("id", "en_scraped", "jp_official", "jp_scraped")
-final$en_scraped <- str_replace(final$en_scraped, "\\[", "") #beware formatting of some cards, e.g. GH Collab
-final$en_scraped <- str_replace(final$en_scraped, "\\]", "")
+final$en_scraped <- str_replace(final$en_scraped, "\\[", "") #remove brackets surrounding uncertain translations
+final$en_scraped <- str_replace(final$en_scraped, "\\]", "") #but beware formatting of some cards, e.g. GH Collab
 #final <- subset(final, !is.na(en_scraped)) #easier for copy paste
 
 View(final) #final data table to double check work
-nrow(dff) #number of new cards
-nrow(df3) #check to see if it matches up with amount scraped
-cat(paste("^credit", final$id, final$en_scraped), sep="\n") #output to console
+nrow(new_cards_df) #number of new cards
+nrow(translated) #check to see if it matches up with amount scraped
+cat(paste("^credit", final$id, final$en_scraped), sep="\n") #output lines for pasting to console
